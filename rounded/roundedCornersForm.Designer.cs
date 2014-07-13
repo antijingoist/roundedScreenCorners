804433de90a18434d1025c15c564a8fafd5796af
Abelardo Gonzalez <abbie@abbiecod.es>
2014-07-13 11:36:47 -0400

    Initial commit
    Initial commit

---
+++ rounded/roundedCornersForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace rounded
+{
+    partial class roundedCornersForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(roundedCornersForm));
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.roundedCornerFramePictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.roundedCornerFramePictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // roundedCornerFramePictureBox
+            // 
+            this.roundedCornerFramePictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.roundedCornerFramePictureBox.BackColor = System.Drawing.Color.Lime;
+            this.roundedCornerFramePictureBox.Location = new System.Drawing.Point(0, -1);
+            this.roundedCornerFramePictureBox.Name = "roundedCornerFramePictureBox";
+            this.roundedCornerFramePictureBox.Size = new System.Drawing.Size(199, 152);
+            this.roundedCornerFramePictureBox.TabIndex = 4;
+            this.roundedCornerFramePictureBox.TabStop = false;
+            // 
+            // roundedCornersForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(198, 150);
+            this.Controls.Add(this.roundedCornerFramePictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "roundedCornersForm";
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "roundedCorners";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.Lime;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.roundedCornersForm_Load);
+            this.SizeChanged += new System.EventHandler(this.roundedCornersForm_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.roundedCornerFramePictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Windows.Forms.PictureBox roundedCornerFramePictureBox;
+    }
+}
+
---
+++ rounded/roundedCornersForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rounded
+{
+    public partial class roundedCornersForm : Form
+    {
+        public roundedCornersForm()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void roundedCornersForm_Load(object sender, EventArgs e)
+        {
+            drawRoundedCorners();
+        }
+        private void drawRoundedCorners()
+        {
+            roundedCornerFramePictureBox.Image = null;
+            GraphicsPath roundedRectPath = new GraphicsPath();
+            Rectangle rectanglePath = new Rectangle(0, 0, roundedCornerFramePictureBox.Width, roundedCornerFramePictureBox.Height);
+            int roundedCornerDiameter = 50;
+
+            roundedRectPath.AddArc(rectanglePath.X, rectanglePath.Y, roundedCornerDiameter, roundedCornerDiameter, 180, 90);
+
+            roundedRectPath.AddArc(rectanglePath.X + rectanglePath.Width - roundedCornerDiameter,
+                                    rectanglePath.Y, roundedCornerDiameter, roundedCornerDiameter, 270, 90);
+
+            roundedRectPath.AddArc(rectanglePath.X + rectanglePath.Width - roundedCornerDiameter,
+                                    rectanglePath.Y + rectanglePath.Height - roundedCornerDiameter,
+                                    roundedCornerDiameter, roundedCornerDiameter, 0, 90);
+
+            roundedRectPath.AddArc(rectanglePath.X, rectanglePath.Y + rectanglePath.Height - roundedCornerDiameter,
+                                    roundedCornerDiameter, roundedCornerDiameter, 90, 90);
+
+            roundedCornerFramePictureBox.Region = new Region(roundedRectPath);
+        }
+
+        private void roundedCornersForm_SizeChanged(object sender, EventArgs e)
+        {
+            drawRoundedCorners();
+        }
+    }
+}
